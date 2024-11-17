a8a732cdb492d23efcb4b7f2c9ea3ce97b27bf38
mfarhann10 <mfarhann1009@gmail.com>
2024-11-17 17:24:50 +0700

    add the close button

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const messages = [
 ];
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
     if (step < messages.length - 1) {
       return null;
@@ -29,16 +30,30 @@ function App() {
   const notActiveStep =
     "w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center text-lg font-bold text-gray-700";
   return (
-    <div className="steps bg-gray-100 max-w-lg mx-auto mt-24 p-8 rounded-xl shadow-lg space-y-8">
-      {/* Steps */}
-      <Step step={step} activeStep={activeStep} notActiveStep={notActiveStep} />
+    <>
+      <button
+        className="absolute top-4 right-4 border-none bg-transparent cursor-pointer text-4xl text-current hover:text-indigo-600"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="bg-gray-100 max-w-lg mx-auto mt-24 p-8 rounded-xl shadow-lg space-y-8">
+          {/* Steps */}
+          <Step
+            step={step}
+            activeStep={activeStep}
+            notActiveStep={notActiveStep}
+          />
 
-      {/* Message */}
-      <Message step={step} message={messages} />
+          {/* Message */}
+          <Message step={step} message={messages} />
 
-      {/* Buttons */}
-      <Button onClickPrevious={handlePrevious} onClickNext={handleNext} />
-    </div>
+          {/* Buttons */}
+          <Button onClickPrevious={handlePrevious} onClickNext={handleNext} />
+        </div>
+      )}
+    </>
   );
 }
 
